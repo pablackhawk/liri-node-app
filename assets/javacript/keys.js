ac9fbf9a5b9e83b4aecf51a30a15b85a6bebad1e
pablackhawk <l.tirtarahardja@gmail.com>
2017-12-05 23:47:09 -0800

    Initial file setup

---
+++ assets/javacript/keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded')
+
+var twitterKeys = {
+  consumer_key: 'EKdw2oNYgV4M11pDpUDDfphdg',
+  consumer_secret: 'AEHFIbm4OUmPZIdlbrfNlecQDGQZc20BYOtXL6IMT3Y9LUEhqo',
+  access_token_key: '130607645-WcS0DyrTS4DNJvtS8IgmRH3U16rBYuLBilMrBJ7A',
+  access_token_secret: 'MInU2zDiHlTdXlRsoWOBgcx7829JM3CyO9ITBHd8UjQBy'
+}
+
+module.exports = twitterKeys
+
+var spotifyKeys = {
+  client_key: 'bf782e382cd740cb9454c7f0f80edcfc',
+  client_secret: '752db995fda0454684c92d0e0df7dc22'
+}
